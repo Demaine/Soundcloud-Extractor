40c589a426a88cf0d635b24eb9f7062ab1a5d724
Colin Demaine <demaine3@hotmail.com>
2019-11-01 18:31:44 +0100

    Initial Commit - Grab titles only

---
+++ soundcloud-extractor.js
@@ -0,0 +1,11 @@
+javascript:(function(){
+  var targets=document.querySelectorAll(".soundTitle__titleContainer");
+  let s="";
+  for (i=0; i<targets.length; i++) {
+    var title=targets[i].querySelector(".soundTitle__title").innerText;
+    s+=`<div><a href="#">${title}</a></div>`;
+  }
+  myWindow=window.open('','');
+  myWindow.document.write(s);
+  myWindow.focus();
+})()
