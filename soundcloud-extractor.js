e07b1aadd0106b72eaa88bd76c5eee2c792e3433
Colin Demaine <demaine3@hotmail.com>
2020-11-01 00:04:59 +0100

    Add early exit condition

--- soundcloud-extractor.js
+++ soundcloud-extractor.js
@@ -1,12 +1,17 @@
-var targets=document.querySelectorAll(".userMain .soundTitle__titleContainer");
-let s="";
-for(i=0; i<targets.length; i++){
-  var item=targets[i].querySelector(".soundTitle__title");
-  var title=item.innerText;
-  var link=item.href;
-  var date=item.querySelector(".relativeTime").dateTime;
-  s+=`<div><a href="${link}">${title}</a>[${date}]</div>`;
+if (location.hostname == "soundcloud.com"){
+  var targets=document.querySelectorAll(".userMain .soundTitle__titleContainer");
+  let s="";
+  for(i=0; i<targets.length; i++){
+    var item=targets[i].querySelector(".soundTitle__title");
+    var title=item.innerText;
+    var link=item.href;
+    var date=item.querySelector(".relativeTime").dateTime;
+    s+=`<div><a href="${link}">${title}</a>[${date}]</div>`;
+  }
+  myWindow=window.open('','');
+  myWindow.document.write(s);
+  myWindow.focus();
 }
-myWindow=window.open('','');
-myWindow.document.write(s);
-myWindow.focus();
+else {
+  alert("This extractor only works on Soundcloud.com!");
+}
