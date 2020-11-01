75333cb8651279b0fa2a1b1b9db341a0cf7fd8c9
Colin Demaine <demaine3@hotmail.com>
2020-11-01 02:11:48 +0100

    Add feature to load the whole Soundcloud feed automatically

--- soundcloud-extractor.js
+++ soundcloud-extractor.js
@@ -1,20 +1,32 @@
 if (location.hostname == "soundcloud.com"){
-  var targets=document.querySelectorAll(".userMain .soundTitle__titleContainer");
-  if (targets.length > 0) {
-    let s="";
-    for(i=0; i<targets.length; i++){
-      var item=targets[i].querySelector(".soundTitle__title");
-      var title=item.innerText;
-      var link=item.href;
-      var date=targets[i].querySelector(".relativeTime").dateTime.slice(0,10);
-      s+=`<div><a href="${link}">${title}</a>[${date}]</div>`;
+  var loadFeed = setInterval(function() {
+    let more = document.querySelector(".loading");
+    if (more) {
+      document.querySelector(".loading").scrollIntoView();
     }
-    myWindow=window.open('','');
-    myWindow.document.write(s);
-    myWindow.focus();
-  }
-  else {
-    alert("Error: No tracks found!");
+    else {
+      clearInterval(loadFeed);
+      getTracks();
+    }
+  }, 1000);
+  function getTracks() {
+    var targets=document.querySelectorAll(".userMain .soundTitle__titleContainer");
+    if (targets.length > 0) {
+      let s="";
+      for(i=0; i<targets.length; i++){
+        var item=targets[i].querySelector(".soundTitle__title");
+        var title=item.innerText;
+        var link=item.href;
+        var date=targets[i].querySelector(".relativeTime").dateTime.slice(0,10);
+        s+=`<div><a href="${link}">${title}</a>[${date}]</div>`;
+      }
+      myWindow=window.open('','');
+      myWindow.document.write(s);
+      myWindow.focus();
+    }
+    else {
+      alert("Error: No tracks found!");
+    }
   }
 }
 else {
